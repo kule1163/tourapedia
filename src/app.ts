782b41e30803edacfc8ff67f6c1c195f71c49d65
Ahmen Batuhan KIR <79552743+kule1163@users.noreply.github.com>
2022-07-23 21:41:54 +0300

    make it better

--- src/app.ts
+++ src/app.ts
@@ -42,11 +42,11 @@ mongoose
   .catch((err) => console.log(err.message));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, "../my-app/build")));
 
-  console.log(path.join(__dirname, "../client/build"));
+  console.log(path.join(__dirname, "../my-app/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+    res.sendFile(path.join(__dirname, "../my-app/build/index.html"));
   });
 }
