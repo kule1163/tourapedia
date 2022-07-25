b80749e625cb24a85d6a93567624853d885a124b
Ahmen Batuhan KIR <79552743+kule1163@users.noreply.github.com>
2022-07-26 00:20:26 +0300

    make it better

--- src/app.ts
+++ src/app.ts
@@ -44,7 +44,7 @@ if (process.env.NODE_ENV === "production") {
 const MONGODB_URI = process.env.MONGODB_URI!;
 const PORT = process.env.PORT || 5000;
 
-if (MONGODB_URI) {
+/* if (MONGODB_URI) {
   console.log(process.env.MONGODB_URI, process.env.PORT);
 
   mongoose
@@ -53,4 +53,4 @@ if (MONGODB_URI) {
       app.listen(PORT, () => console.log(`server is listeninggg ${PORT}`))
     )
     .catch((err) => console.log(err.message));
-}
+} */
