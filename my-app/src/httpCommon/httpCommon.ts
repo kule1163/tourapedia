ea85668664af3710481599cc30e37eba45b98628
Ahmen Batuhan KIR <79552743+kule1163@users.noreply.github.com>
2022-07-26 02:02:37 +0300

    make it better

--- my-app/src/httpCommon/httpCommon.ts
+++ my-app/src/httpCommon/httpCommon.ts
@@ -1,24 +1,24 @@
 import axios from "axios";
 
 export const httpAuth = axios.create({
-  baseURL: "http://localhost:5000/auth",
+  baseURL: "/auth",
   headers: {
     "Content-Type": `application/x-www-form-urlencoded`,
   },
 });
 
 export const httpPosts = axios.create({
-  baseURL: "http://localhost:5000/posts",
+  baseURL: "/posts",
 });
 
 export const httpComment = axios.create({
-  baseURL: "http://localhost:5000/comment",
+  baseURL: "/comment",
 });
 
 export const httpLike = axios.create({
-  baseURL: "http://localhost:5000/like",
+  baseURL: "/like",
 });
 
 export const httpDislike = axios.create({
-  baseURL: "http://localhost:5000/dislike",
+  baseURL: "/dislike",
 });
