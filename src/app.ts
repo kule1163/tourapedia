8a01f2889883bcd7d01bf2b5c0d0ebc1d62a5f57
Ahmen Batuhan KIR <79552743+kule1163@users.noreply.github.com>
2022-07-28 01:52:45 +0300

    make it better

--- src/app.ts
+++ src/app.ts
@@ -32,10 +32,10 @@ app.use("/comment", commentRoute);
 app.use("/like", likeRoute);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../../my-app/build")));
+  app.use(express.static(path.join(__dirname, "../my-app/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../my-app/build/index.html"));
+    res.sendFile(path.join(__dirname, "../my-app/build/index.html"));
   });
 }
 
