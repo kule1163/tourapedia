8ac4fd291155f8e2a8451613cb47289971ba7515
Ahmen Batuhan KIR <79552743+kule1163@users.noreply.github.com>
2022-07-28 18:01:03 +0300

    make it better

--- my-app/src/httpCommon/httpCommon.ts
+++ my-app/src/httpCommon/httpCommon.ts
@@ -2,23 +2,25 @@ import axios from "axios";
 
 export const httpAuth = axios.create({
   baseURL: "/auth",
-  headers: {
-    "Content-Type": `application/x-www-form-urlencoded`,
-  },
+  headers: { "Content-Type": "multipart/form-data" },
 });
 
 export const httpPosts = axios.create({
   baseURL: "/posts",
+  headers: { "Content-Type": "multipart/form-data" },
 });
 
 export const httpComment = axios.create({
   baseURL: "/comment",
+  headers: { "Content-Type": "multipart/form-data" },
 });
 
 export const httpLike = axios.create({
   baseURL: "/like",
+  headers: { "Content-Type": "multipart/form-data" },
 });
 
 export const httpDislike = axios.create({
   baseURL: "/dislike",
+  headers: { "Content-Type": "multipart/form-data" },
 });
